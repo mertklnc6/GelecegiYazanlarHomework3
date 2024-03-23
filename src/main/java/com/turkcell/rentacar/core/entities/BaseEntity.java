3a6debae501c073dd864280de678f5da5e0a4987
Ahmethan1 <ahmethansafa@hotmail.com>
2024-03-23 23:18:59 +0300

    update

--- src/main/java/com/turkcell/rentacar/core/entities/BaseEntity.java
+++ src/main/java/com/turkcell/rentacar/core/entities/BaseEntity.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@MappedSuperclass
+@MappedSuperclass //super Class
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
